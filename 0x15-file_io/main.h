cafb999f46a228491c9f27a6d381b7ab9640ee45
youness-mchaar <younessmchaar1999@gmail.com>
2023-05-09 03:20:17 -0700

    HELLO ALLs

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x15-file_io directory.
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif /* MAIN_H */
